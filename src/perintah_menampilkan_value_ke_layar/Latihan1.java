c8641b633ae7b617f19bd92ec20ca272d9d0ba01
Dilan <ramadhanrizqi21@gmail.com>
2020-10-20 22:38:42 +0700

    first commit

---
+++ src/perintah_menampilkan_value_ke_layar/Latihan1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perintah_menampilkan_value_ke_layar;
+
+/**
+ *
+ * @author Muhammad Alvin Rizqi Ramadhan
+ * @author NIM 10119913
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" +"------------------" );
+        System.out.println("NAMA\t\t: Muhammad Alvin Rizqi Ramadhan");
+        System.out.println("ALAMAT\t\t: Komp Padasuka Indah no 48A");
+        System.out.println("TEMPAT LLAHIR\t\t: Bandung");
+        System.out.println("TANGGAL LAHIRT\t\t: 03 Desember 2000");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t: Mountaineering and Hiking");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Teh");
+                
+    }
+    
+}
